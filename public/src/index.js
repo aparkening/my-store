0cbd08a292ed9ff1162b649add211d05030551b5
aparkening <aaron.parkening@gmail.com>
2020-06-18 17:24:03 -0400

    Add conditional: don't write root style if it already exists

--- public/src/index.js
+++ public/src/index.js
@@ -2,13 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Change base color in head css
   const baseColor = () => {
-    let css = ':root { --main-bg-color: green; }',
-    head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
+    // Only take action if style tag doesn't already exist
+    if (!document.head.getElementsByTagName('style').length) {
+      let css = ':root { --main-bg-color: green; }',
+      head = document.head || document.getElementsByTagName('head')[0],
+      style = document.createElement('style');
 
-    head.appendChild(style);
-    style.type = 'text/css';
-    style.appendChild(document.createTextNode(css));
+      head.appendChild(style);
+      style.type = 'text/css';
+      style.appendChild(document.createTextNode(css)); 
+    }
   }
 
   // Change css colors on button click
